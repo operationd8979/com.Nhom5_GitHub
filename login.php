70075030b57faf06fcb11f9f5b60174864a8575e
Võ Hoàng Dũng <127659280+operationd8979@users.noreply.github.com>
2024-04-14 23:40:29 +0700

    Update login.php

--- login.php
+++ login.php
@@ -33,19 +33,15 @@ $loginController->invoke($error);
                     <input type="password" id="password" name="password" value="12345678"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 </div>
-                <div class="mb-4 flex items-center justify-between">
-                    <div class="captcha">
-                        <img src="captcha.php" alt="Captcha" onclick="this.src='captcha.php?refresh=1'">
-                    </div>
-                    <input type="text" id="captcha" name="captcha"
-                        class="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Enter Captcha">
+                 <div class="mb-4">
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                    <input type="email" id="email" name="email" value="operationddd@gmail.com"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 </div>
-                <div class="flex items-center justify-between">
-                    <button type="submit"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign
-                        In</button>
-                    <a href="register.php" class="text-blue-500 hover:text-blue-800 text-sm">Sign up?</a>
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                    <input type="password" id="password" name="password" value="12345678"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 </div>
             </form>
         </div>
