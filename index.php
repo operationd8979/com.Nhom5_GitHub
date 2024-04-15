b440e8cf5cfccf477f70724986296382be87c0b6
operationd8979 <operationddd@gmail.com>
2024-04-15 07:43:33 +0700

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TipRecipePhp</title>
+    <title>TipRecipePhpThu2</title>
 </head>
 
 
